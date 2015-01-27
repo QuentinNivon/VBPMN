b730199ae3415978fac05ee7a4d01a6e341a0dcc
Pascal Poizat <pascal.poizat@lip6.fr>
2015-01-27 18:33:02 +0100

    test for reading files

--- tests/java/pif/ProcessTest.java
+++ tests/java/pif/ProcessTest.java
@@ -5,8 +5,11 @@ import org.testng.annotations.Test;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 import static org.testng.Assert.*;
 
@@ -43,12 +46,34 @@ public class ProcessTest {
             ctx = JAXBContext.newInstance(Process.class);
             final Marshaller marshaller = ctx.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            // marshaller.marshal(p, fos);
+            marshaller.marshal(p, fos);
         } catch (JAXBException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void test_readFromFile() {
+        Process p;
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream("/Users/pascalpoizat/IdeaProjects/vbpmn/examples/p0000.pif");
+            JAXBContext ctx = JAXBContext.newInstance(Process.class);
+            Unmarshaller unmarshaller = ctx.createUnmarshaller();
+            p = (Process) unmarshaller.unmarshal(fis);
+            fis.close();
+            assertEquals(p.getName(),"p0000");
+            assertEquals(p.getBehaviour().getNodes().size(),8);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
