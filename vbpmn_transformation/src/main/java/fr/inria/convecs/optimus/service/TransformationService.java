c2615d50956c9b52e62f627b2da0337c439456b3
Ajay Krishna <ajaykrishnamn@gmail.com>
2021-06-07 23:34:00 +0200

    Added BPMN to PIF service

---
+++ vbpmn_transformation/src/main/java/fr/inria/convecs/optimus/service/TransformationService.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package fr.inria.convecs.optimus.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.inria.convecs.optimus.config.AppProperty;
+import fr.inria.convecs.optimus.transformer.ContentTransformer;
+import fr.inria.convecs.optimus.transformer.PifContentTransformer;
+import fr.inria.convecs.optimus.util.VbpmnExceptionMapper;
+
+/**
+ * @author ajayk
+ *
+ */
+@Path("/transform")
+public class TransformationService {
+
+	private static final Logger logger = LoggerFactory.getLogger(TransformationService.class);
+
+	private static final String OUTPUT_PATH = AppProperty.getInstance().getFolder("OUTPUT_PATH");
+
+	private static final String SCRIPTS_PATH = "/WEB-INF/classes";
+
+	private static final String PIF_SCHEMA = "/pif.xsd";
+
+	@Context ServletContext servletContext;
+
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces("text/plain")
+	@Path("/pif2bpmn")
+	public Response validateVbpmn(@FormDataParam("file1") InputStream fileStream1,
+			@FormDataParam("file1") FormDataContentDisposition fileInfo1, FormDataMultiPart formData) {
+
+		Response httpResponse = null;
+		try {      
+
+			String outputDir = Files.createTempDirectory(Paths.get(OUTPUT_PATH), "vbpmn_")
+					.toAbsolutePath().toString();
+
+			File pifInput = new File(outputDir + File.separator + fileInfo1.getFileName());
+			Files.copy(fileStream1, pifInput.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			
+			File bpmnOutput = new File(outputDir + File.separator + fileInfo1.getFileName()+".bpmn");
+			
+			ContentTransformer pifContentTransformer = new PifContentTransformer(pifInput, bpmnOutput);
+			pifContentTransformer.transform();
+			
+			String bpmnResult = IOUtils.toString(new FileInputStream(bpmnOutput), StandardCharsets.UTF_8);
+			
+			httpResponse = Response.status(Status.OK).entity(bpmnResult).build();
+			
+			return httpResponse;
+
+		} catch (Exception e) {
+			logger.error("Error processing request: ", e);
+			throw VbpmnExceptionMapper.createWebAppException(e);
+		}
+	}
+}
--- vbpmn_transformation/src/main/webapp/media/js/verify.js
+++ vbpmn_transformation/src/main/webapp/media/js/verify.js
@@ -1,167 +1,236 @@
 /**
  * app related scripts
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-	window.visualise = function(container, dotData, edgeColor, nodeColor){  
-		var container = document.getElementById(container);
-		var parsedData = vis.network.convertDot(dotData);
-		var data = {
-				nodes: parsedData.nodes,
-				edges: parsedData.edges
-		}
+    window.visualise = function (container, dotData, edgeColor, nodeColor) {
+        var container = document.getElementById(container);
+        var parsedData = vis.network.convertDot(dotData);
+        var data = {
+            nodes: parsedData.nodes,
+            edges: parsedData.edges
+        }
 
-		//console.log(data);
+        //console.log(data);
 
-		var options = parsedData.options;
+        var options = parsedData.options;
 
-		options.nodes = {
-				color: nodeColor
-		}
-		options.edges = {
-				color: edgeColor
-		}
+        options.nodes = {
+            color: nodeColor
+        }
+        options.edges = {
+            color: edgeColor
+        }
 
-		for(edge of data.edges)
-		{
-			var label = edge.label;
-			if(label.includes("Present in"))
-			{
-				edge.color = 'blue';
-				data.nodes[edge.to].color = 'lime';
-			}
-			if(label.includes("Absent in"))
-			{
-				edge.color = 'blue';
-				data.nodes[edge.to].color = 'red';
-			}
-		}
+        for (edge of data.edges) {
+            var label = edge.label;
+            if (label.includes("Present in")) {
+                edge.color = 'blue';
+                data.nodes[edge.to].color = 'lime';
+            }
+            if (label.includes("Absent in")) {
+                edge.color = 'blue';
+                data.nodes[edge.to].color = 'red';
+            }
+        }
 
-		var network = new vis.Network(container, data, options);
+        var network = new vis.Network(container, data, options);
 
-		return network;
-	}  
+        return network;
+    }
 
-	$("#resp-div").hide();
-	//$("#formula-div").hide();
-	$("#hide-rename-div").hide();
+    $("#resp-div").hide();
+    //$("#formula-div").hide();
+    $("#hide-rename-div").hide();
 
-	$("#ltsdisplay").hide();
+    $("#ltsdisplay").hide();
 
-//	$("#mode").val("conservative").change();
-//	$( "#noneOption" ).prop( "checked", true);
+    //	$("#mode").val("conservative").change();
+    //	$( "#noneOption" ).prop( "checked", true);
 
-	var status = null;
-	var model1 = null;
-	//var model2 = null;
-	var counterExample = null;
+    var status = null;
+    var model1 = null;
+    //var model2 = null;
+    var counterExample = null;
 
-	$("#vform").submit(function(event){
+    $("#vform").submit(function (event) {
 
-		//disable the default form submission
-		event.preventDefault();
+        //disable the default form submission
+        event.preventDefault();
 
-		//grab all form data  
-		var formData = new FormData($(this)[0]);
+        //grab all form data  
+        var formData = new FormData($(this)[0]);
 
-		$("#resp-div").hide();
-		$("#graphContainer").html("");
-		$("#graphContainer").hide();
-		$("#model1Container").html("");
-		$("#model1Container").hide();
-		$.ajax({
-			url: 'http://localhost:8080/transformation/vbpmn/validate/verify',
-			type: 'POST',
-			data: formData,
-			cache: false,
-			beforeSend: function () {
-				$("#loader").show();
-				if($("#response").hasClass("alert alert-success"))
-					$("#response").removeClass("alert alert-success");
-				if($("#response").hasClass("alert alert-danger"))
-					$("#response").removeClass("alert alert-danger");
-				if($("#response").hasClass("alert alert-warning"))
-					$("#response").removeClass("alert alert-warning");
-			},
-			contentType: false,
-			processData: false,
-			error: function (returnData) {
-				$("#loader").hide();
-				$("#resp-div").show();
-				$("#response").text(returnData.responseText); 
-				$("#ltsbtn").hide();
-				$("#resetVis").hide();
-				$('label[for="graphContainer"]').hide();
-				$('label[for="model1Container"]').hide();
-				$("#response").addClass("alert alert-danger");
-			},
-			success: function (returnData) {
-				var resp = returnData.trim();
-				status = resp.split('|');
-				var ceContainer = 'graphContainer';
-				$("#loader").hide();
-				$("#resp-div").show();
-				$("#response").text(status[0]);
-				console.log(status)
-				if(status[0].trim().toUpperCase() === "TRUE") {
-					$("#response").addClass("alert alert-success");
-					$("#model1Container").show();
-					$('label[for="graphContainer"]').hide();
-					$("#ltsdisplay").hide();
-					$("#resetVis").hide();
-				}
-				else if (status[0].trim().toUpperCase() === "FALSE") {
-					$("#graphContainer").show();
-					$("#model1Container").show();
-					$('label[for="graphContainer"]').show();
-					$("#ltsdisplay").hide();
-					$("#resetVis").show();
-					counterExample = visualise(ceContainer, status[3], 'black', 'lightgrey');
-					$("#response").addClass("alert alert-danger");
-				}
-				else {
-					$("#response").html("<br /><p><strong>ERROR</strong> Unable to process. Please contact the team</p><br /><p>"+resp+"</p>");
-					$("#response").addClass("alert alert-warning");
-					$("#ltsbtn").hide();
-					$("#resetVis").hide();
-					$('label[for="graphContainer"]').hide();
-					$('label[for="model2Container"]').hide();
-				}
-			}
-		});
+        $("#resp-div").hide();
+        $("#graphContainer").html("");
+        $("#graphContainer").hide();
+        $("#model1Container").html("");
+        $("#model1Container").hide();
+        $.ajax({
+            url: 'http://localhost:8080/transformation/vbpmn/validate/verify',
+            type: 'POST',
+            data: formData,
+            cache: false,
+            beforeSend: function () {
+                $("#loader").show();
+                if ($("#response").hasClass("alert alert-success"))
+                    $("#response").removeClass("alert alert-success");
+                if ($("#response").hasClass("alert alert-danger"))
+                    $("#response").removeClass("alert alert-danger");
+                if ($("#response").hasClass("alert alert-warning"))
+                    $("#response").removeClass("alert alert-warning");
+            },
+            contentType: false,
+            processData: false,
+            error: function (returnData) {
+                $("#loader").hide();
+                $("#resp-div").show();
+                $("#response").text(returnData.responseText);
+                $("#ltsbtn").hide();
+                $("#resetVis").hide();
+                $('label[for="graphContainer"]').hide();
+                $('label[for="model1Container"]').hide();
+                $("#response").addClass("alert alert-danger");
+            },
+            success: function (returnData) {
+                var resp = returnData.trim();
+                status = resp.split('|');
+                var ceContainer = 'graphContainer';
+                $("#loader").hide();
+                $("#resp-div").show();
+                $("#response").text(status[0]);
+                console.log(status)
+                if (status[0].trim().toUpperCase() === "TRUE") {
+                    $("#response").addClass("alert alert-success");
+                    $("#model1Container").show();
+                    $('label[for="graphContainer"]').hide();
+                    $("#ltsdisplay").hide();
+                    $("#resetVis").hide();
+                }
+                else if (status[0].trim().toUpperCase() === "FALSE") {
+                    $("#graphContainer").show();
+                    $("#model1Container").show();
+                    $('label[for="graphContainer"]').show();
+                    $("#ltsdisplay").hide();
+                    $("#resetVis").show();
+                    counterExample = visualise(ceContainer, status[3], 'black', 'lightgrey');
+                    $("#response").addClass("alert alert-danger");
+                }
+                else {
+                    $("#response").html("<br /><p><strong>ERROR</strong> Unable to process. Please contact the team</p><br /><p>" + resp + "</p>");
+                    $("#response").addClass("alert alert-warning");
+                    $("#ltsbtn").hide();
+                    $("#resetVis").hide();
+                    $('label[for="graphContainer"]').hide();
+                    $('label[for="model2Container"]').hide();
+                }
+            }
+        });
 
-		$('#ltscheckbox').change(function() {
-			var model1Container ='model1Container';
-			//var model2Container = 'model2Container';
-			if($(this).prop('checked'))
-			{
-				$("#ltsdisplay").show();
-				$("#resetVis").show();
-				$("#model1Container").html("");
-				model1 = visualise(model1Container, status[1], 'saddlebrown', 'peachpuff');
-				model2 = visualise(model2Container, status[2], 'saddlebrown', 'peachpuff');
-			}
-			else {
-				$("#ltsdisplay").hide();
-				if(status[0].trim().toUpperCase() === "TRUE")
-				{
-					$("#resetVis").hide();
-				}
-			}
+        $('#ltscheckbox').change(function () {
+            var model1Container = 'model1Container';
+            //var model2Container = 'model2Container';
+            if ($(this).prop('checked')) {
+                $("#ltsdisplay").show();
+                $("#resetVis").show();
+                $("#model1Container").html("");
+                model1 = visualise(model1Container, status[1], 'saddlebrown', 'peachpuff');
+                model2 = visualise(model2Container, status[2], 'saddlebrown', 'peachpuff');
+            }
+            else {
+                $("#ltsdisplay").hide();
+                if (status[0].trim().toUpperCase() === "TRUE") {
+                    $("#resetVis").hide();
+                }
+            }
 
-		});
+        });
 
-		//event.unbind();
-		$('#resetVis').click(function() {
-			if(null != counterExample)
-				counterExample.fit();
-			if(null != model1)
-				model1.fit();
-		});
+        //event.unbind();
+        $('#resetVis').click(function () {
+            if (null != counterExample)
+                counterExample.fit();
+            if (null != model1)
+                model1.fit();
+        });
 
 
-		return false;
-	});
+        return false;
+    });
 
+    $("#tform").submit(function (event) {
 
+        //disable the default form submission
+        event.preventDefault();
+    
+        //grab all form data  
+        var formData = new FormData($(this)[0]);
+    
+        $("#resp-div").hide();
+        $("#graphContainer").html("");
+        $("#graphContainer").hide();
+        $("#model1Container").html("");
+        $("#model1Container").hide();
+        $.ajax({
+            url: 'http://localhost:8080/transformation/vbpmn/transform/pif2bpmn',
+            type: 'POST',
+            data: formData,
+            cache: false,
+            beforeSend: function () {
+                $("#loader").show();
+                if ($("#response").hasClass("alert alert-success"))
+                    $("#response").removeClass("alert alert-success");
+                if ($("#response").hasClass("alert alert-danger"))
+                    $("#response").removeClass("alert alert-danger");
+                if ($("#response").hasClass("alert alert-warning"))
+                    $("#response").removeClass("alert alert-warning");
+            },
+            contentType: false,
+            processData: false,
+            error: function (returnData) {
+                $("#loader").hide();
+                $("#resp-div").show();
+                $("#response").text(returnData.responseText);
+                $("#ltsbtn").hide();
+                $("#resetVis").hide();
+                $('label[for="graphContainer"]').hide();
+                $('label[for="model1Container"]').hide();
+                $("#response").addClass("alert alert-danger");
+            },
+            success: function (returnData) {
+                var resp = returnData.trim();
+                console.log(resp)
+                status = resp.split('|');
+                var ceContainer = 'graphContainer';
+                $("#loader").hide();
+                $("#resp-div").show();
+                $("#response").text(status[0]);
+                console.log(status)
+                if (status[0].trim().toUpperCase() === "TRUE") {
+                    $("#response").addClass("alert alert-success");
+                    $("#model1Container").show();
+                    $('label[for="graphContainer"]').hide();
+                    $("#ltsdisplay").hide();
+                    $("#resetVis").hide();
+                }
+                else if (status[0].trim().toUpperCase() === "FALSE") {
+                    $("#graphContainer").show();
+                    $("#model1Container").show();
+                    $('label[for="graphContainer"]').show();
+                    $("#ltsdisplay").hide();
+                    $("#resetVis").show();
+                    counterExample = visualise(ceContainer, status[3], 'black', 'lightgrey');
+                    $("#response").addClass("alert alert-danger");
+                }
+                else {
+                    $("#response").html("<br /><p><strong>ERROR</strong> Unable to process. Please contact the team</p><br /><p>" + resp + "</p>");
+                    $("#response").addClass("alert alert-warning");
+                    $("#ltsbtn").hide();
+                    $("#resetVis").hide();
+                    $('label[for="graphContainer"]').hide();
+                    $('label[for="model2Container"]').hide();
+                }
+            }
+        });
+    });
 });
