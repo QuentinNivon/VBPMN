a6fe73681e8c3d6d4583d68e7e01122fcdc45fa5
quentin <quentin.nivon@yahoo.fr>
2024-09-11 15:43:45 +0200

    cleaning main

--- vbpmn_transformation/src/main/java/fr/inria/convecs/optimus/nl_to_mc/Filename.java
+++ vbpmn_transformation/src/main/java/fr/inria/convecs/optimus/nl_to_mc/Filename.java
@@ -4,22 +4,6 @@ import java.io.File;
 
 public class Filename
 {
-	public static final String BCG_SPECIFICATION = "problem.bcg";
-	public static final String BRANCHED_BCG_SPECIFICATION = "problem_branched.bcg";
-	public static final String BCG_SPECIFICATION_WEAK = "problem_weak.bcg";
-	public static final String AUT_SPECIFICATION = "problem.aut";
-	public static final String AUT_SPECIFICATION_WEAK = "problem_weak.aut";
-	public static final String BCG_PRODUCT = "product.bcg";
-	public static final String BRANCHED_BCG_PRODUCT = "product_branched.bcg";
-	public static final String WEAK_BCG_PRODUCT = "product_weak.bcg";
-	public static final String WEAK_AUT_PRODUCT = "product_weak.aut";
-	public static final String GRAPH_3D_CLTS = "clts.graph3D";
-	public static final String AUT_CLTS = "clts.aut";
-	public static final String BCG_CLTS = "clts.bcg";
-	public static final String BRANCHED_BCG_CLTS = "clts_branched.bcg";
-	public static final String WEAK_BCG_CLTS = "clts_weak.bcg";
-	public static final String AUT_FULL_CLTS = "clts_full.aut";
-	public static final String AUTX_FULL_CLTS = "clts_full.autx";
 	public static final String COUNTEREXAMPLE_FILE = "diag";
 	public static final String TEMPORARY_COUNTEREXAMPLE = COUNTEREXAMPLE_FILE + ".tmp";
 	public static final String VBPMN_COUNTEREXAMPLE_FILE = "evaluator.bcg";
